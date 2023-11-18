9cbce1bd73f12d043c7bbfd67c2fdf70b9c65386
Ramesh Kumar <rameshkumar455555@gmailcom>
2023-11-18 07:50:42 +0530

    solved bug

--- CricketScore/settings.py
+++ CricketScore/settings.py
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_COOKIE_SECURE = True
 # settings.py
 
-CSRF_TRUSTED_ORIGINS = ['https://t20scorepridictor-production.up.railway.app']
+#CSRF_TRUSTED_ORIGINS = ['https://t20scorepridictor-production.up.railway.app']
 
